84f927c052b32a54fd378623079506d2054a33d4
Jerry W <jerrywiltse@gmail.com>
2017-07-31 20:49:48 -0400

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,24 @@
+from conans import ConanFile, tools, os
+
+class BoostPreprocessorConan(ConanFile):
+    name = "Boost.Preprocessor"
+    version = "1.64.0"
+    generators = "txt"
+    url = "https://github.com/boostorg/preprocessor"
+    description = "For a description of this library, please visit http://boost.org/preprocessor "
+    license = "www.boost.org/users/license.html"
+    lib_short_name = "preprocessor"
+
+    def source(self):
+        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
+                 .format(self.version, self.url))
+
+    def package(self):
+        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        self.copy(pattern="*.h", dst="", src=include_dir)
+        self.copy(pattern="*.hpp", dst="", src=include_dir)
+        self.copy(pattern="*.ipp", dst="", src=include_dir)
+        
+    def package_info(self):
+        self.cpp_info.libs = [self.lib_short_name]
+
---
+++ upload.sh
@@ -0,0 +1,1 @@
+conan upload --all --remote bincrafters Boost.Preprocessor/1.64.0@bincrafters/testing
