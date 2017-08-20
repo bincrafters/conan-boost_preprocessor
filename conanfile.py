6909d8c50934b478afc92f2719fc63cdf4152a41
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:53 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostPreprocessorConan(ConanFile):
     name = "Boost.Preprocessor"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-preprocessor"
     source_url = "https://github.com/boostorg/preprocessor"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -11,7 +12,7 @@ class BoostPreprocessorConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
