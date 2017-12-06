e54033c471a4893559ea7dd8533b036d995d0d08
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:48 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,30 +4,29 @@ from conans import ConanFile, tools
 class BoostPreprocessorConan(ConanFile):
     name = "Boost.Preprocessor"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-preprocessor"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
     lib_short_names = ["preprocessor"]
     is_header_only = True
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-preprocessor"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
 
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
         return super(self.__class__, self).env
+
+    def package_id(self):
+        self.info.header_only()
 
     # END
