2cd6e3071e0eca8092ea4ecf7ead1030a3f961f0
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:00:51 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostPreprocessorConan(ConanFile):
     name = "boost_preprocessor"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_preprocessor"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -17,7 +17,7 @@ class BoostPreprocessorConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing"
     )
     
     # BEGIN
@@ -25,7 +25,7 @@ class BoostPreprocessorConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
