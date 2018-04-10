73e152a5f91931e5c84578d9d5f1b684982214c6
Rene Rivera <grafikrobot@gmail.com>
2018-04-10 09:47:10 -0500

    Initial 1.67.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,7 @@ from conans import ConanFile, tools
 
 class BoostPreprocessorConan(ConanFile):
     name = "boost_preprocessor"
-    version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost_preprocessor"
+    version = "1.67.0"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     lib_short_names = ["preprocessor"]
@@ -17,15 +16,16 @@ class BoostPreprocessorConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.67.0@bincrafters/testing"
     )
-    
+
     # BEGIN
 
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    url = "https://github.com/bincrafters/conan-boost_preprocessor"
+    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.67.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
@@ -53,7 +53,5 @@ class BoostPreprocessorConan(ConanFile):
             import boost_package_tools  # pylint: disable=F0401
             boost_package_tools.package_info(self)
         getattr(self, "package_info_additional", lambda:None)()
-
-
 
     # END
