872f0be7553a721aad1aa3a499cb8a5924d32b9a
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,10 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostPreprocessorConan(base.BoostBaseConan):
     name = "boost_preprocessor"
     url = "https://github.com/bincrafters/conan-boost_preprocessor"
     lib_short_names = ["preprocessor"]
     header_only_libs = ["preprocessor"]
-
